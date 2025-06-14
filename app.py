96d350ecb53724fa8d655e9265533f3d113300ed
masiniluca <lucamasini05@gmail.com>
2025-06-14 14:09:17 +0200

    Psitola codice a barre

--- app.py
+++ app.py
@@ -150,4 +150,5 @@ def rimuovi_prodotto():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
